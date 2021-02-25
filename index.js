560fdf00eca0e3f4bdad1ff2f72db42d348df2b3
Ricky Klusmeier <rickyklusmeier@Rickys-MacBook-Pro.local>
2021-02-25 13:16:12 -0700

    Task 2 constructor

--- index.js
+++ index.js
@@ -81,7 +81,16 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon){
+      this.model = model,
+      this.milesPerGallon = milesPerGallon,
+      this.tank = 0,
+      this.odometer = 0
+    }
+
+    fill(gallons){
+      gallons += this.tank;
+    }
   }
   
   /*
